1f148ffe0a5f68f95c5f643d7b4e3fa9bc71d546
Ferhat Oguncer <oguncerr26@gmail.com>
2025-10-10 21:19:57 +0300

    problem 28 refactored

--- problem28.py
+++ problem28.py
@@ -1,13 +1,35 @@
+# k = 1001
+# size = 3  
+# # size 3,5,7,9,11 ... diye artar 
+# sum = 1
+# i = 1 #şuanda 1.sayıdayız.
+
+# while size < k+1:
+#     for n in range(4):  # for 4 edge numbers in each square
+#         i += size - 1  # şuanda merkezdeki 1in değeri
+#         sum += i
+#     size += 2
+
+# print(sum)
+
+
+#refactoring code for nested loop.
+
+
 k = 1001
 size = 3  
 # size 3,5,7,9,11 ... diye artar 
 sum = 1
 i = 1 #şuanda 1.sayıdayız.
-
+count = 0
 while size < k+1:
-    for n in range(4):  # for 4 edge numbers in each square
-        i += size - 1  # şuanda merkezdeki 1in değeri
-        sum += i
-    size += 2
+    
+    i += size - 1  # şuanda merkezdeki 1in değeri
+    sum += i
+    count += 1
+    if count == 4: # for 4 edge numbers in each square
+
+        size += 2
+        count = 0
 
 print(sum)
