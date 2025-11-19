104cf12c51ae2f1a94cf81430855b99b0b59a10b
controltwone <oguncerr26@gmail.com>
2025-11-19 23:00:02 +0300

    additional solve was appended to  problem67 max. path sum.

--- problem67.py
+++ problem67.py
@@ -9,17 +9,28 @@ triangle = [[int(word) for word in line.split()]for line in data.strip().split("
 
 
 
+# def solution_from_botton_to_top():
+  
+#   for row in reversed(range(len(triangle)-1)):
+#     row_ = triangle[row]
+    
+#     for col in range(len(row_)):
+#       row_[col] += max(triangle[row+1][col], triangle[row+1][col+1]) 
+      
+#   return triangle[0][0]
+
+# print(solution_from_botton_to_top())
+
+
 def solution_from_botton_to_top():
   
   for row in reversed(range(len(triangle)-1)):
-    row_ = triangle[row]
+   
     
-    for col in range(len(row_)):
-      row_[col] += max(triangle[row+1][col], triangle[row+1][col+1]) 
+    for col in range(row+1):
+      triangle[row][col] += max(triangle[row+1][col], triangle[row+1][col+1]) 
       
   return triangle[0][0]
 
 print(solution_from_botton_to_top())
-
-
 
