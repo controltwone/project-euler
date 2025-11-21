13ecd1f4ced4611cafd01ddcf871b5959e4d2823
controltwone <oguncerr26@gmail.com>
2025-11-21 22:51:59 +0300

    problem44 solved.

--- problem44.py
+++ problem44.py
@@ -1,16 +1,20 @@
 
-def pentagonal(n : int) ->  int :
-    num = n*(3*n - 1)/2
-    return num
+def is_pentagon(num):
+    pentagon = str((((num*24 +1)**(1/2)+1)/6))
+    if pentagon[-1] == "0":
+        return True
+    return False
 
-pent = []
-for i in range(1,10000):
-    pent.append(pentagonal(i))
+def pentagon(num):
+    return num*(3*num-1)/2
+    
+def solution():   
+    for x in range(10,10000):
+        for y in range(x,10000):
+            num1 = pentagon(x)
+            num2 = pentagon(y)    
 
-
-for  x in range(len(pent)):
-    for y in range(len(pent)):
-        if x + y in pent and abs(x-y) in pent:
-            sol = abs(x - y)
-            minimum = min(sol,10000)
-print(minimum)
+            if is_pentagon(num1 + num2) == True and is_pentagon(num2 - num1) == True:
+                return num2 - num1
+            
+print(solution())
