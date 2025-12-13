288fb441602a98d80008a044c727a46aae69d5ab
controltwone <oguncerr26@gmail.com>
2025-12-13 22:23:33 +0300

    solved PE57 using recursion methot.

--- problem57.py
+++ problem57.py
@@ -1,12 +1,22 @@
 from fractions import Fraction
 
-print(Fraction(0.25))
+import sys
+sys.setrecursionlimit(2000)  # Limiti 2000'e çıkar
 
 
-def recursion(num):
+def square_root(num):
     if num == 0:
         return 0
 
-    return 1/(2 + recursion(num-1))
+    return Fraction(1, 2 + square_root(num - 1))
 
-print(recursion(1000))
+
+def sol():
+    count = 0
+    for x in range(1,1001):
+        value = 1 + square_root(x)
+        if len(str(value.numerator)) > len(str(value.denominator)):   
+            count += 1
+    return count
+
+print(sol())
